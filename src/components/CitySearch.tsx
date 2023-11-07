29300be532774c8a6b1e9729c020db8dacc69a64
yurii-k <37714822+yurii-k@users.noreply.github.com>
2023-11-07 11:36:48 +0200

    added error handler to fetchCities

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -1,6 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { Alert } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import { useEffect, useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
 import { City } from '../shared/models.ts';
 import { fetchCities } from '../services/api.service.ts';
 
@@ -10,6 +12,9 @@ export default function CitySearch({ city, onSelectCity }) {
   const [value, setValue] = useState<City | null>(city);
   const [inputValue, setInputValue] = useState('');
   const [options, setOptions] = useState<readonly City[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   useEffect(() => {
     if (inputValue === '') {
@@ -20,11 +25,23 @@ export default function CitySearch({ city, onSelectCity }) {
     fetchCities(inputValue)
       .then((results: City[]) => {
         setOptions(results);
+        setError(null);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        setError('Error fetching cities. Please try again later.');
+        console.error(error);
+        setSnackbarOpen(true);
+      });
 
     return () => {};
   }, [value, inputValue]);
+
+  const vertical = 'bottom';
+  const horizontal = 'center';
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
 
   useEffect(() => {
     setValue(city);
@@ -32,32 +49,52 @@ export default function CitySearch({ city, onSelectCity }) {
   }, [city]);
 
   return (
-    <Autocomplete
-      sx={{ width: 300 }}
-      filterOptions={(x) => x}
-      options={options}
-      autoComplete
-      includeInputInList
-      filterSelectedOptions
-      value={value}
-      onChange={(event: any, newValue: City | null) => {
-        setOptions(newValue ? [newValue, ...options] : options);
-        setValue(newValue);
-        onSelectCity(newValue);
-      }}
-      onInputChange={(event, newInputValue) => {
-        setInputValue(newInputValue);
-      }}
-      renderInput={(params) => <TextField {...params} label="City" />}
-      getOptionLabel={(option) => option.name}
-      isOptionEqualToValue={(option, value) => option.name === value.name}
-      renderOption={(props, option) => {
-        return (
-          <li {...props} key={`${option.lat}-${option.lon}-${option.isHistoryItem}`}>
-            {option.name}({option.country})
-          </li>
-        );
-      }}
-    />
+    <div>
+      {error && (
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={4000}
+          anchorOrigin={{ vertical, horizontal }}
+          onClose={handleCloseSnackbar}
+        >
+          <Alert
+            severity="error"
+            variant="filled"
+            sx={{ width: '100%' }}
+            onClose={handleCloseSnackbar}
+          >
+            {error}
+          </Alert>
+        </Snackbar>
+      )}
+
+      <Autocomplete
+        sx={{ width: 300 }}
+        filterOptions={(x) => x}
+        options={options}
+        autoComplete
+        includeInputInList
+        filterSelectedOptions
+        value={value}
+        onChange={(event: any, newValue: City | null) => {
+          setOptions(newValue ? [newValue, ...options] : options);
+          setValue(newValue);
+          onSelectCity(newValue);
+        }}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        renderInput={(params) => <TextField {...params} label="City" />}
+        getOptionLabel={(option) => option.name}
+        isOptionEqualToValue={(option, value) => option.name === value.name}
+        renderOption={(props, option) => {
+          return (
+            <li {...props} key={`${option.lat}-${option.lon}-${option.isHistoryItem}`}>
+              {option.name}({option.country})
+            </li>
+          );
+        }}
+      />
+    </div>
   );
 }
